0fc7dba8608e8d10414e7768a23d86b9e6a480c3
Malisha Perera <malishashavinda577@gmail.com>
2025-08-28 13:26:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import {Button} from "@/components/ui/button.jsx";
+
+function App() {
+
+  return (
+    <>
+
+        <main>
+            <h1 className="text-3xl font-bold underline bg-amber-200">
+                Hello world!
+            </h1>
+            <Button>hello</Button>
+        </main>
+    </>
+  )
+}
+
+export default App
