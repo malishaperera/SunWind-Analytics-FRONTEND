549dcc4b531bc88e8d49274e6aa29c221ea98f33
Malisha Perera <malishashavinda577@gmail.com>
2025-11-13 12:42:47 +0530

    feat(api): add authorization token handling in baseQuery and new endpoint for user solar units🔐

--- src/lib/redux/query.js
+++ src/lib/redux/query.js
@@ -6,14 +6,31 @@ const baseUrl = "http://localhost:3000/api";
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
         reducerPath: 'api',
-        baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
+        baseQuery: fetchBaseQuery(
+            {
+                baseUrl: baseUrl,
+                prepareHeaders:async (headers) => {
+                    const clerk = window.Clerk;
+                    if (clerk){
+                        const token = await clerk.session.getToken();
+                        console.log("Retrieved token:", token);
+                        if (token) {
+                            headers.set('authorization', `Bearer ${token}`);
+                        }
+                    }
+                    return headers;
+                }
+            }),
         endpoints: (build) => ({
             getEnergyGenerationRecordsBySolarUnit: build.query({
             query: ({id,groupBy,limit}) => `/energy-generation-records/solar-unit/${id}?groupBy=${groupBy}&limit=${limit}`,
+            }),
+            getSolarUnitForUser: build.query({
+            query: () => `/solar-units/me`,
             }),
     }),
     })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetEnergyGenerationRecordsBySolarUnitQuery  } = api;
+export const { useGetEnergyGenerationRecordsBySolarUnitQuery ,useGetSolarUnitForUserQuery } = api;
