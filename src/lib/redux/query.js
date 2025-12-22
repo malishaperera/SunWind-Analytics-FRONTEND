1e8bd61b23018f51579f9ba171793b079081c761
Malisha Perera <malishashavinda577@gmail.com>
2025-12-22 13:22:49 +0530

    feat(query): update API base URL to use environment variable for better configuration✅

--- src/lib/redux/query.js
+++ src/lib/redux/query.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl = "http://localhost:3000/api";
+// const baseUrl = "http://localhost:3000/api";
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 // Define a service using a base URL and expected endpoints
 // export const api = createApi({
