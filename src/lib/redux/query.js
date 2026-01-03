dd1d989e11251d721d1a073e571635a2ceeda24a
Malisha Perera <malishashavinda577@gmail.com>
2026-01-03 23:00:36 +0530

    fix(query): remove unnecessary blank line in API configuration

--- src/lib/redux/query.js
+++ src/lib/redux/query.js
@@ -2,7 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 // const baseUrl = "http://localhost:3000/api";
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
-
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
